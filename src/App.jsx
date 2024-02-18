6a51c0299c0796d526593803b42ef783514ebd53
TSNAGA <tarundeep.nagla85@gmail.com>
2024-02-19 02:28:37 +0530

    create and run the server

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+  <BrowserRouter>
+  <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/sign-in' element={<SignIn />} />
@@ -16,4 +19,5 @@ export default function App() {
       <Route path='/profile' element={<Profile />} />
     </Routes>
   </BrowserRouter>
+  );
 }
